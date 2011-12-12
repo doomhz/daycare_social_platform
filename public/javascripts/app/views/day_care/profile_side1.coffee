3fcddb728d8a904889219151207945897122d3b7
Dumitru Glavan <dumitru.gl@gmail.com>
2011-12-12 20:10:24 +0100

    Send quick message box

--- public/javascripts/app/views/day_care/profile_side1.coffee
+++ public/javascripts/app/views/day_care/profile_side1.coffee
@@ -4,8 +4,13 @@ class window.Kin.DayCare.ProfileSide1View extends Backbone.View
 
   tplUrl: '/templates/side1/day_care/profile.html'
   
+  quickMessageTplUrl: '/templates/side1/day_care/quick_message_box.html'
+  
   selectedMenuItem: null
 
+  events:
+    "click #quick-message-bt": "quickMessageHandler"
+  
   initialize: ({@selectedMenuItem})->
     @model and @model.view = @
     @
@@ -22,3 +27,24 @@ class window.Kin.DayCare.ProfileSide1View extends Backbone.View
     @unbind()
     $(@el).unbind().empty()
     @
+
+  quickMessageHandler: (ev)->
+    ev.preventDefault()
+    @showQuickMessageWindow()
+  
+  showQuickMessageWindow: ()->
+    that = @
+    $.tmpload
+      url: @quickMessageTplUrl
+      onLoad: (tpl)->
+        winContent = tpl({dayCare: that.model})
+        dWindow(winContent, {
+          closeOnSideClick: false
+          buttons:
+            "send":   "send"
+            "cancel": "cancel"
+          buttonClick: (btType, $win)->
+            if btType is "send"
+              $.jGrowl("Message sent!")
+            $win.close()
+        })
--- public/javascripts/app/views/day_care/profile_side1.js
+++ public/javascripts/app/views/day_care/profile_side1.js
@@ -14,7 +14,11 @@
     }
     ProfileSide1View.prototype.el = null;
     ProfileSide1View.prototype.tplUrl = '/templates/side1/day_care/profile.html';
+    ProfileSide1View.prototype.quickMessageTplUrl = '/templates/side1/day_care/quick_message_box.html';
     ProfileSide1View.prototype.selectedMenuItem = null;
+    ProfileSide1View.prototype.events = {
+      "click #quick-message-bt": "quickMessageHandler"
+    };
     ProfileSide1View.prototype.initialize = function(_arg) {
       this.selectedMenuItem = _arg.selectedMenuItem;
       this.model && (this.model.view = this);
@@ -38,6 +42,36 @@
       this.unbind();
       $(this.el).unbind().empty();
       return this;
+    };
+    ProfileSide1View.prototype.quickMessageHandler = function(ev) {
+      ev.preventDefault();
+      return this.showQuickMessageWindow();
+    };
+    ProfileSide1View.prototype.showQuickMessageWindow = function() {
+      var that;
+      that = this;
+      return $.tmpload({
+        url: this.quickMessageTplUrl,
+        onLoad: function(tpl) {
+          var winContent;
+          winContent = tpl({
+            dayCare: that.model
+          });
+          return dWindow(winContent, {
+            closeOnSideClick: false,
+            buttons: {
+              "send": "send",
+              "cancel": "cancel"
+            },
+            buttonClick: function(btType, $win) {
+              if (btType === "send") {
+                $.jGrowl("Message sent!");
+              }
+              return $win.close();
+            }
+          });
+        }
+      });
     };
     return ProfileSide1View;
   })();
