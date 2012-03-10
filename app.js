907da1b896652c44503a6170c845231698d713ce
Dumitru Glavan <dumitru.gl@gmail.com>
2012-03-10 21:11:07 +0100

    Do not detect changes

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var app = module.exports = express.createServer(
   express.session({secret: 'kinsecretkey83', store: new RedisStore({db: redisAuthDbName})}),
   require('stylus').middleware({src: __dirname + '/public'}),
   //express.static(__dirname + '/public'),
-  require('connect-assets')({src: __dirname + '/public'}),
+  require('connect-assets')({src: __dirname + '/public', detectChanges: false}),
   gzippo.staticGzip(__dirname + '/public'),
   mongooseAuth.middleware()
 );
